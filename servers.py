314b53dee1dfd25dd3782506636f5422408f38ad
michalsciubisz <michalsci@student.agh.edu.pl>
2021-11-08 21:29:56 +0100

    update
    Latest

--- servers.py
+++ servers.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from typing import Optional, List
+from abc import ABC, abstractmethod
 import re #biblioteka do poszukiwania patternu
 
 #product skończony najprawdopodobniej
@@ -17,7 +18,7 @@ class Product:
         return hash((self.name, self.price))
 
 
-class Server:
+class Server(ABC): #klasa abstraktcyjna dziedziczy po abstrakcyjnej klasie ABC
     n_max_returned_entries: int = 3
 
     def __init__(self, *kwargs, **args):
@@ -39,7 +40,7 @@ class Server:
     def get_all_products(self, n_letters: int = 1):
         return NotImplementedError()
 
-class TooManyProductsFoundError:
+class TooManyProductsFoundError(Exception):
     # Reprezentuje wyjątek związany ze znalezieniem zbyt dużej liczby produktów.
     pass
 
@@ -49,16 +50,46 @@ class TooManyProductsFoundError:
 #   (2) możliwość odwołania się do atrybutu klasowego `n_max_returned_entries` (typu int) wyrażający maksymalną dopuszczalną liczbę wyników wyszukiwania,
 #   (3) możliwość odwołania się do metody `get_entries(self, n_letters)` zwracającą listę produktów spełniających kryterium wyszukiwania
 
+
+#dwie funkcje do uzupełnienia
 class ListServer:
-    pass
+    def __init__(self):
+        pass
+
+    def get_all_products(self, n_letters: int = 1):
+        pass
 
 
 class MapServer:
-    pass
+    def __init__(self):
+        pass
+
+    def get_all_products(self, n_letters: int = 1):
+        pass
 
 
 class Client:
-    def __init__(self):
-        server: Server
+    def __init__(self, server: Server):
+        self.server: Server = server
     def get_total_price(self, n_letters: Optional[int]) -> Optional[float]:
         raise NotImplementedError()
+
+
+class Product:
+    # TODO: zweryfikowac warunek na nazwe(1 litera/ 1 cyfra) // warunek na unikalna reprezentacje tego samego produktu
+    # FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą argumenty wyrażające nazwę produktu (typu str) i jego cenę (typu float) -- w takiej kolejności -- i ustawiającą atrybuty `name` (typu str) oraz `price` (typu float)
+    products = None
+
+    def __init__(self, name: str, price: float):
+        if re.fullmatch('[a-zA-Z] + [0-9]+', name):
+            self.name = name
+            self.price = price
+            # self.name_char = [i for i in name]
+        else:
+            raise ValueError
+
+    def __eq__(self, other):
+        return self.price == other.price and self.name == other.name  # FIXME: zwróć odpowiednią wartość
+
+    def __hash__(self):
+        return hash((self.name, self.price))
